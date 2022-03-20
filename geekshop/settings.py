07487c276afd1742fdc9484d1827a14cac01a999
Artem <a2291067@gmail.com>
2022-03-21 00:38:29 +0300

    Lesson 4. User login and logout

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
-    'authnapp',
+    "authnapp",
 ]
 
 MIDDLEWARE = [
@@ -138,4 +138,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Auth model
-AUTH_USER_MODEL = 'authnapp.ShopUser'
+AUTH_USER_MODEL = "authnapp.ShopUser"
